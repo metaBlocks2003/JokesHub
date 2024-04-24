0245d26d795cace82d64307e133b60a54b92cf8e
unknown <you@example.com>
2024-04-24 21:26:38 +0530

    Url fixed

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.get('/getRandomJoke', (req, res) => {
           </a>
           <p class="mb-3 font-normal text-xl text-blue-200">${j.punchline}</p>
           <div class="py-6">
-          <a href="http://localhost:5000/getRandomJoke" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <a href="https://jokeshub-dtx0.onrender.com/getRandomJoke" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Generate One More
           </a>
           </div>
-          <a href="http://localhost:5000" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <a href="https://jokeshub-dtx0.onrender.com" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Go Back
       </a>
       </div>
